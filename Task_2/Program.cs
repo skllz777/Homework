ba25d0e1c7b6919bee7de30a4144e78490371a04
Oleg <tradecenter1771@gmail.com>
2022-11-21 15:28:42 +0300

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    if (n > k)
+    Console.WriteLine($"max: {n}");
+    else
+    Console.WriteLine($"max: {k}");
+else if (m > k)
+Console.WriteLine($"max: {m}");
+else 
+Console.WriteLine($"max: {k}");
