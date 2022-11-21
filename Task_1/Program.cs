147a5ea2a1c62b22b0765988e72ceb96ea32f725
Oleg <tradecenter1771@gmail.com>
2022-11-21 15:24:28 +0300

    add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+    if (n > m)
+    Console.WriteLine($"max: {n}");
+    else
+    if (m > n)
+    Console.WriteLine($"max: {m}");
+    else Console.WriteLine("Числа равны");
