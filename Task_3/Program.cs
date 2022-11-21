410f9bf99bada553698329430487cc81a5618f08
Oleg <tradecenter1771@gmail.com>
2022-11-21 15:31:18 +0300

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 1)
+Console.WriteLine("Число является нечётным");
+else
+Console.WriteLine("Число является чётным");
